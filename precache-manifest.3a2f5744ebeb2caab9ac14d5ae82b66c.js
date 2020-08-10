a6beff40ec06672bd0a965d77279266afcb0220c
lychee.tw <lychee.tw+gitlab@gmail.com>
2020-08-11 02:40:11 +0800

    Updates

--- precache-manifest.3a2f5744ebeb2caab9ac14d5ae82b66c.js
+++ precache-manifest.3a2f5744ebeb2caab9ac14d5ae82b66c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd3db586b306ab6223bc45dbf5e30d1e",
+    "revision": "643405f1eb7145078affcfaa9d5363d8",
     "url": "/index.html"
   },
   {
-    "revision": "58f2f466331570b7d515",
+    "revision": "e97f68a64b08558422fc",
     "url": "/static/css/main.1765d0d8.chunk.css"
   },
   {
-    "revision": "387bbdc2bb16b482dcae",
-    "url": "/static/js/2.da2ccbd3.chunk.js"
+    "revision": "03aa4d6648b5bdc4f29f",
+    "url": "/static/js/2.1d6d29b9.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.da2ccbd3.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.1d6d29b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58f2f466331570b7d515",
-    "url": "/static/js/main.591f4045.chunk.js"
+    "revision": "e97f68a64b08558422fc",
+    "url": "/static/js/main.cce5e7f0.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a93ad87e111ee7473697864d8de7224d.js"
+  "/precache-manifest.3a2f5744ebeb2caab9ac14d5ae82b66c.js"
 );
 
 self.addEventListener('message', (event) => {
